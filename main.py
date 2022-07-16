ccdc37cae06d9e5e24e940b16c3d23b0ea884814
bel-senpaii <bellsenpaii@gmail.com>
2022-07-16 23:52:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 
 
-print ("\tWelcome to PyRPG!") #title mthfcka
+print ("\tWelcome to PyRPG!") #title
 
-print ("Enter your name, stranger: ") #account name. sort of.
+print ("Enter your name, stranger: ") #get player name
 name = input()
 name = name.strip()
 
 print (f"So, {name.title()} ,welcome aboard!") #greetings
 
-characters = ['Mage', 'Warrior', 'Rouge']
+characters = ['Mage', 'Warrior', 'Rouge'] #list of characters
 print(characters)
-player_char = input("Choose your character: ")
-player_char = player_char.title()
+
+player_char = input("Choose your character: ") #player inputs character class
+
+player_char = player_char.title() #sets player's first letter of input to uppercase to prevent errors
+
+
+#confirm what character player is choosen
 if player_char == 'Mage':
 	print(f"So, {name.title()}, you want to be a Mage? Good choce!")
 elif player_char == 'Warrior':
